70da17d3a33de9a2f9fecc1f474d35480d9b7bcd
Argiris Anastasakis <anastasakis.an@gmail.com>
2022-05-19 14:42:09 +0300

    Create native javascript todo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 window.onload = () => {
-  console.log("Hi");
+  let inputField = document.querySelector("input.li-input-field");
+  let createLiButton = document.querySelector("button.create-list-item-button");
+  let ul = document.querySelector("ul.my-list");
+
+  createLiButton.addEventListener("click", addListItem);
+
+  function addListItem(){
+    let li = document.createElement("li");
+    let deleteButton = document.createElement("button");
+    deleteButton.innerText = " x ";
+    deleteButton.addEventListener("click", deleteListItem);
+    li.innerText = inputField.value;
+    li.appendChild(deleteButton);
+    ul.appendChild(li);
+    inputField.value = "";
+  }
+
+  function deleteListItem(){
+    let closestLi = this.closest("li");
+    ul.removeChild(closestLi);
+  }
 };
