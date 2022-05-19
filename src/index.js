f08b057338349e790663a1938a69e4013254b8eb
Argiris Anastasakis <anastasakis.an@gmail.com>
2022-05-19 17:00:19 +0300

    Show list items count below the button

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ window.onload = () => {
   let inputField = document.querySelector("input.li-input-field");
   let createLiButton = document.querySelector("button.create-list-item-button");
   let ul = document.querySelector("ul.my-list");
+  let ulChildrenCount = document.querySelector(".list-items-count");
+  let pluralSyntax = document.querySelector(".plural-items-syntax");
+
+  showListChildrenCount();
 
   createLiButton.addEventListener("click", addListItem);
   inputField.addEventListener("keydown", handleInputField);
@@ -25,6 +29,7 @@ window.onload = () => {
     li.appendChild(deleteButton);
     ul.appendChild(li);
     refreshInputField();
+    showListChildrenCount();
   }
 
   function refreshInputField() {
@@ -35,5 +40,11 @@ window.onload = () => {
   function deleteListItem(){
     let closestLi = this.closest("li");
     ul.removeChild(closestLi);
+    showListChildrenCount();
+  }
+
+  function showListChildrenCount(){
+    ulChildrenCount.innerText = ul.children.length;
+    pluralSyntax.innerText = ul.children.length === 1 ? "" : "s"
   }
 };
