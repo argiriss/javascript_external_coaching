0435e6c8e6af206d8c1df458785b63dac3f41e8a
Argiris Anastasakis <anastasakis.an@gmail.com>
2022-05-23 16:16:46 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,53 +1,5 @@
 import './style/main.scss';
 
 window.onload = () => {
-  let inputField = document.querySelector("input.li-input-field");
-  let createLiButton = document.querySelector("button.create-list-item-button");
-  let ul = document.querySelector("ul.my-list");
-  let ulChildrenCount = document.querySelector(".list-items-count");
-  let pluralSyntax = document.querySelector(".plural-items-syntax");
-
-  showListChildrenCount();
-
-  createLiButton.addEventListener("click", addListItem);
-  inputField.addEventListener("keydown", handleInputField);
-
-  function handleInputField(event) {
-    if (event.key == "Enter") {
-      addListItem();
-    }
-  }
-
-  function addListItem(){
-    if (inputField.value === "") {
-      return false;
-    }
-    let li = document.createElement("li");
-    li.className = "list-item";
-    let deleteButton = document.createElement("button");
-    deleteButton.addEventListener("click", deleteListItem);
-    deleteButton.className = "delete-button";
-    deleteButton.innerHTML = "<i class='fas fa-trash'></i>";
-    li.innerHTML = "<span class='list-item-text'>"+inputField.value+"</span>";
-    li.appendChild(deleteButton);
-    ul.appendChild(li);
-    refreshInputField();
-    showListChildrenCount();
-  }
-
-  function refreshInputField() {
-    inputField.value = "";
-    inputField.focus();
-  }
-
-  function deleteListItem(){
-    let closestLi = this.closest("li");
-    ul.removeChild(closestLi);
-    showListChildrenCount();
-  }
-
-  function showListChildrenCount(){
-    ulChildrenCount.innerText = ul.children.length;
-    pluralSyntax.innerText = ul.children.length === 1 ? "" : "s"
-  }
+  
 };
