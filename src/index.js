ae37515b760d7d9319917e67939a7a5d2d84e3f2
Argiris Anastasakis <anastasakis.an@gmail.com>
2022-05-25 16:59:59 +0300

    Prettier format on js file

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
-import "./style/main.scss";
-import { makeRequest, formatDate } from "./utils";
+import './style/main.scss';
+import { makeRequest, formatDate } from './utils';
 
-let inputField = document.querySelector("input.movieInputField");
-let yearInputField = document.querySelector("input.yearInputField");
-let searchMoviesButton = document.querySelector("button.searchMoviesButton");
-let moviesList = document.querySelector(".moviesList");
+let inputField = document.querySelector('input.movieInputField');
+let yearInputField = document.querySelector('input.yearInputField');
+let searchMoviesButton = document.querySelector('button.searchMoviesButton');
+let moviesList = document.querySelector('.moviesList');
 
 window.onload = async () => {
-  searchMoviesButton.addEventListener("click", function () {
+  searchMoviesButton.addEventListener('click', function () {
     searchMovies();
   });
 
-  inputField.addEventListener("keydown", handleInputField);
+  inputField.addEventListener('keydown', handleInputField);
 };
 
 function addLoader() {
-  let loaderDiv = document.createElement("div");
+  let loaderDiv = document.createElement('div');
   loaderDiv.innerHTML = "<span class='loader'>Loading...</span>";
   moviesList.appendChild(loaderDiv);
 }
 
 function removeLoader() {
-  let loader = document.querySelector(".loader");
+  let loader = document.querySelector('.loader');
   loader.remove();
 }
 
 function handleInputField(event) {
-  if (event.key == "Enter") {
+  if (event.key == 'Enter') {
     searchMovies();
   }
 }
 
 async function searchMovies() {
   addLoader();
-  if (inputField.value === "") {
+  if (inputField.value === '') {
     removeLoader();
     return false;
   }
@@ -52,27 +52,25 @@ async function searchMovies() {
 
 function populateMoviesList(moviesResults) {
   removeLoader();
-  moviesList.innerHTML = "";
+  moviesList.innerHTML = '';
 
-  if (moviesResults.Response === "False") {
-    let errorDiv = document.createElement("div");
-    errorDiv.innerHTML =
-      "<span class='error'>" + moviesResults.Error + "</span>";
+  if (moviesResults.Response === 'False') {
+    let errorDiv = document.createElement('div');
+    errorDiv.innerHTML = "<span class='error'>" + moviesResults.Error + '</span>';
     moviesList.appendChild(errorDiv);
   } else {
     let movies = moviesResults.Search;
     for (let movie of movies) {
-      let newMovie = document.createElement("li");
-      newMovie.className = "searchResult";
-      newMovie.setAttribute("data-imdbId", movie.imdbID);
-      newMovie.innerHTML =
-        "<span class='movieTitle'>" + movie.Title + "</span>";
-      newMovie.addEventListener("click", function () {
+      let newMovie = document.createElement('li');
+      newMovie.className = 'searchResult';
+      newMovie.setAttribute('data-imdbId', movie.imdbID);
+      newMovie.innerHTML = "<span class='movieTitle'>" + movie.Title + '</span>';
+      newMovie.addEventListener('click', function () {
         displayMovie(movie.imdbID);
       });
 
-      let img = document.createElement("img");
-      img.className = "moviePoster";
+      let img = document.createElement('img');
+      img.className = 'moviePoster';
       img.src = movie.Poster;
       img.alt = movie.Title;
       newMovie.appendChild(img);
@@ -92,18 +90,17 @@ async function displayMovie(imdbID) {
 
 function showMovieInfo(movieResults, imdbID) {
   let movie = document.querySelector(`[data-imdbId="${imdbID}"]`);
-  if (movie.lastChild.className === "movieInfo") {
+  if (movie.lastChild.className === 'movieInfo') {
     movie.lastChild.remove();
-  }
-  else {
-    let movieInfo = document.createElement("div");
-    movieInfo.className = "movieInfo";
+  } else {
+    let movieInfo = document.createElement('div');
+    movieInfo.className = 'movieInfo';
     movieInfo.innerHTML = movieResults.Plot;
     movie.appendChild(movieInfo);
   }
 }
 
 function clearResults() {
-  inputField.value = "";
+  inputField.value = '';
   inputField.focus();
 }
